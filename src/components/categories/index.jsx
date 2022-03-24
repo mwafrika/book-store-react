6d427e6b70e9dfb064d6554a61351fcd7e4f2815
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-03-24 16:24:36 +0200

    Add check status for the category

--- src/components/categories/index.jsx
+++ src/components/categories/index.jsx
@@ -1,10 +1,14 @@
+/* eslint-disable jsx-quotes */
 import React from 'react';
 import Header from '../header';
 
 const index = () => (
   <>
     <Header />
-    <div>Categories</div>
+    <div className='flex justify-start items-center ml-5 gap-x-4'>
+      <input type='checkbox' id='status' value='status' />
+      <h1>Check status</h1>
+    </div>
   </>
 );
 
